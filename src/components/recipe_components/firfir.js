338a72f07e309b19f8f1c6b141190e296b88576c
veronicaglh <veronicagt.IT@gmail.com>
2023-05-09 00:30:08 +0300

    add recipe component

--- src/components/recipe_components/firfir.js
+++ src/components/recipe_components/firfir.js
@@ -2,7 +2,7 @@
 import React from 'react'; 
 
 // Import statements for css files
-import '../../css/recpie_style.css'; 
+import '../../css/recipe_style.css'; 
 
 
 export default function Firfir() {
