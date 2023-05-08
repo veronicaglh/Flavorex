df89cdd8af58239fcf118817c2ec3d0e2c9ff3f0
veronicaglh <veronicagt.IT@gmail.com>
2023-05-08 23:46:28 +0300

    add starter page

---
+++ src/components/splash_page.js
@@ -0,0 +1,23 @@
+// Starter page of project
+// Import statements 
+import React from 'react';
+
+// Import statements for css files 
+import '../css/splash_style.css';
+import 'swiper/css';
+
+
+export default function SplashPage() {
+  return (
+
+    <div className="splash-div">
+
+      <div className='splash-page'>
+        <h1 className='splash-text'>Flavorex</h1>
+        <p className='splash-text'>TRADITIONAL INGREDIENTS</p>
+      </div>
+
+  </div>
+
+  )
+}
